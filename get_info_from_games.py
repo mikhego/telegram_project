be7f3a7c18f9b1b821cacf6f227a9d582681c4a4
mikhego <egm.fwrk@gmail.com>
2022-10-26 18:17:21 +0300

    correct parsing data

--- get_info_from_games.py
+++ get_info_from_games.py
@@ -1,34 +1,44 @@
 import time
 import requests
 import pandas as pd
+from tqdm import tqdm
+import glob, os
 
-from src.parsing.parsing_functions import (check_data, get_games_info, MODULE_PATH)
+from src.parsing.parsing_functions import (check_data, get_games_info, find_or_create_data_file, MODULE_PATH)
+
+file_path = os.path.join(MODULE_PATH + f'/data/raw/games_info/', 'data_info_*.csv') 
+csv_files = glob.glob(file_path)
 
 
-
-
-
-      
 if __name__ == '__main__':
     check_data()
-    data_games_list = pd.read_csv(MODULE_PATH + '/data/raw/data_games_list.csv')
+    data_games_list: pd.DataFrame = pd.read_csv(MODULE_PATH + '/data/raw/data_games_list.csv')
+    data_info: pd.DataFrame = find_or_create_data_file(filename='/data/raw/data_info.csv', columns=['id', 'tags', 'info', 'tags_info'])
+    set_games_list: set = set(data_games_list['id'].unique())
+    set_games_info: set = set(data_info['id'].unique())
+    diff_set: set = set_games_list.difference(set_games_info)
 
-    number_games = 0
-    while number_games < data_games_list['id'].nunique():
-        try:
-            print('\n time to parse data..')
-            check_list = get_games_info(games_list=data_games_list['id'].unique())
-        except (SystemExit, requests.exceptions.ChunkedEncodingError) as error:
-            print('CONNECTION ERROR (have to wait)')
-            pass
-        temp_df = pd.read_csv(MODULE_PATH + '/data/raw/data_info.csv')
-        number_games = temp_df['id'].nunique()
-        print(f'get games: {number_games} of {data_games_list["id"].nunique()}')
-        if number_games != data_games_list['id'].nunique():
-            print('\n time to sleep (30 min).. z-z-z')
-            time.sleep(1900)
-        else:
-            print('get_games_info COMPLETED')
+    if len(diff_set) > 0:
+        for games_id in tqdm(diff_set):
+            game_file =  MODULE_PATH + f'/data/raw/games_info/data_info_{games_id}.csv'
+            if  game_file in csv_files:
+                pass
+            else:
+                i = 0
+                while i < 3:
+                    try:
+                        get_games_info(games_id=games_id)
+                    except (SystemExit, requests.exceptions.ChunkedEncodingError) as error:
+                        print('CONNECTION ERROR (have to wait)')
+                        print(f'stopped_games_id: {games_id}')
+                        print('\n time to sleep (30 min).. z-z-z')
+                        time.sleep(2100)
+                        continue
+                    i += 1
 
-
-
+        print('get_games_info COMPLETED')
+        print('concat get_games_info to data_info')
+        df_concat = pd.concat([pd.read_csv(f) for f in csv_files], ignore_index=True)
+        df_concat.to_csv(MODULE_PATH + '/data/raw/data_info.csv', index=False)
+    else:
+        print('get_games_info FULL')
